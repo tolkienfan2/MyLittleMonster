8b3b1a97305a983269fcfbf7719d2ce229520ef7
tolkienfan2 <minni.ang@gmail.com>
2016-02-03 14:47:35 -0500

    My Little Monster lesson complete

--- MyLittleMonster/ViewController.swift
+++ MyLittleMonster/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Foundation
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -15,17 +15,138 @@ class ViewController: UIViewController {
 	@IBOutlet weak var foodImg: DragImg!
 	@IBOutlet weak var heartImg: DragImg!
 
+	@IBOutlet weak var penalty1Img: UIImageView!
+	
+	@IBOutlet weak var penalty2Img: UIImageView!
+	
+	@IBOutlet weak var penalty3Img: UIImageView!
+	
+	let DIM_ALPHA: CGFloat = 0.2
+	let OPAQUE: CGFloat = 1.0
+	let MAX_PENALTIES = 3
+	
+	var penalties = 0
+	var timer: NSTimer!
+	var monsterHappy = false
+	var currentItem: UInt32 = 0
+	
+	var musicPlayer: AVAudioPlayer!
+	var sfxBite: AVAudioPlayer!
+	var sfxHeart: AVAudioPlayer!
+	var sfxDeath: AVAudioPlayer!
+	var sfxSkull: AVAudioPlayer!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		foodImg.dropTarget = monsterImg
 		heartImg.dropTarget = monsterImg
+		dimPenalties()
 		
 		NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDropped:", name: "onTargetDropped", object: nil)
+		
+		do {
+			try musicPlayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cave-music", ofType: "mp3")!))
+			try sfxBite = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType: "wav")!))
+			try sfxHeart = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType: "wav")!))
+			try sfxDeath = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType: "wav")!))
+			try sfxSkull = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType: "wav")!))
+			
+			musicPlayer.prepareToPlay()
+			musicPlayer.play()
+			
+			sfxBite.prepareToPlay()
+			sfxHeart.prepareToPlay()
+			sfxDeath.prepareToPlay()
+			sfxSkull.prepareToPlay()
+
+		} catch let error as NSError {
+			print(error.debugDescription)
+		}
+		
+		startTimer()
 	}
 	
 	func itemDropped(notif: AnyObject) {
-		print("item dropped on character")
+		monsterHappy = true
+		startTimer()
+		
+		foodImg.alpha = DIM_ALPHA
+		heartImg.alpha = DIM_ALPHA
+		foodImg.userInteractionEnabled = false
+		heartImg.userInteractionEnabled = false
+		
+		if currentItem == 0 {
+			sfxHeart.play()
+		} else {
+			sfxBite.play()
+		}
+	}
+	
+	func startTimer() {
+		if timer != nil {
+			timer.invalidate()
+		}
+		
+		timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+	}
+	
+	func dimPenalties() {
+		penalty1Img.alpha = DIM_ALPHA
+		penalty2Img.alpha = DIM_ALPHA
+		penalty3Img.alpha = DIM_ALPHA
+	}
+	
+	func changeGameState() {
+		
+		if !monsterHappy {
+			
+			penalties++
+			sfxSkull.play()
+		
+		if penalties == 1 {
+				penalty1Img.alpha = OPAQUE
+			} else if penalties == 2 {
+				penalty2Img.alpha = OPAQUE
+			} else if penalties == 3 {
+				penalty3Img.alpha = OPAQUE
+			} else {
+				dimPenalties()
+			}
+		
+			if penalties >= MAX_PENALTIES {
+				gameOver()
+			}
+		}
+		
+		let rand = arc4random_uniform(2)
+		
+		if rand == 0 {
+			
+			foodImg.alpha = DIM_ALPHA
+			foodImg.userInteractionEnabled = false
+			
+			heartImg.alpha = OPAQUE
+			heartImg.userInteractionEnabled = true
+			
+		} else {
+			
+			foodImg.alpha = OPAQUE
+			foodImg.userInteractionEnabled = true
+			
+			heartImg.alpha = DIM_ALPHA
+			heartImg.userInteractionEnabled = false
+			
+		}
+		
+		currentItem = rand
+		monsterHappy = false
+	}
+	
+	func gameOver() {
+		timer.invalidate()
+		monsterImg.playDeathAnimation()
+		sfxDeath.play()
 	}
 }
 
